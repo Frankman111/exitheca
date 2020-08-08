da3d384250d9d292a451d919c00e82be091b3265
FrankHaider <haider.frank@gmail.com>
2020-08-08 13:39:35 +0200

    color changing

--- resources/views/library/create.blade.php
+++ resources/views/library/create.blade.php
@@ -7,7 +7,7 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">Alle Medien</div>
+                    <div class="card-header" >Alle Medien</div>
 
                     <div class="card-body">
 
